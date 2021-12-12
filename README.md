0f2ade8d8a862683f9186e8a6498b0c60167a015
mkojoa <michaelameyaw7@gmail.com>
2021-12-12 09:06:12 +0000

    init demo

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# AuditBot
-Audit package
+# Welcome to AuditBot
+
+This is a small auditing package
+
+
+The need for auditing already exists in projects, the requirement to log information to prove something happened, but also to capture who, what and when a business action happened.
+
+## Features
--- src/AuditBot/Extention.cs
+++ src/AuditBot/Extention.cs
@@ -1,8 +1,24 @@
 ﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace AuditBot
 {
-    public class Extention
-    { 
+    public static class Extention
+    {
+        public static IServiceCollection AddAuditBot(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services;
+        }
+
+
+        public static IApplicationBuilder UseAuditBot(this IApplicationBuilder app, IConfiguration configuration)
+        {
+            configuration.GetSection("");
+
+            return app;
+        }
+
     }
 }
